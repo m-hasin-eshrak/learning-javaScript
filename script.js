c32fbed95247c5f0cb1b6761f7a7d41c13cf7e18
Hasin Eshrak <m.hasin.eshrak@gmail.com>
2024-12-30 05:30:37 +0600

    up to 9:10

---
+++ script.js
@@ -0,0 +1,24 @@
+/* 1. COMMENTS
+--------------------
+
+// In-Line Comment
+
+ Multi-Line
+Comment */
+
+/* 2. DATA TYPES & VARIABLES
+------------------------------
+undefined, null, boolean, string, symbol, number & object.
+
+3 ways to declare variables
+-> var (global varibale)
+-> let (local variable, can be only used in particular scopes)
+-> const (cannot be changed/replaced/modified)
+
+*/
+
+/* 3. STORING VALUES WITH ASSIGNMENT OPERATORS
+-----------------------------------------------
+
+
+*/
